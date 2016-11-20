e34d61e16fe89d30986eedd349295c6566b92f71
Devon <djp460@nyu.edu>
2016-11-19 22:21:59 -0500

    Initialization of website.

---
+++ js/controllers.js
@@ -0,0 +1,74 @@
+angular.module('app.controllers', [])
+  
+.controller('strykerLoginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('profileSettingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('surgicalBlueprintCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('appendectomyCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('gastrectomyCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('patientDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('patientDetails2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loggedOutCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ js/routes.js
@@ -0,0 +1,95 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('strykerLogin', {
+    url: '/login',
+    templateUrl: 'templates/strykerLogin.html',
+    controller: 'strykerLoginCtrl'
+  })
+
+  .state('menu.profileSettings', {
+    url: '/settings',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/profileSettings.html',
+        controller: 'profileSettingsCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.surgicalBlueprint', {
+    url: '/home',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/surgicalBlueprint.html',
+        controller: 'surgicalBlueprintCtrl'
+      }
+    }
+  })
+
+  .state('menu.appendectomy', {
+    url: '/appendectomy-0730',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/appendectomy.html',
+        controller: 'appendectomyCtrl'
+      }
+    }
+  })
+
+  .state('menu.gastrectomy', {
+    url: '/gastrectomy-1730',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/gastrectomy.html',
+        controller: 'gastrectomyCtrl'
+      }
+    }
+  })
+
+  .state('menu.patientDetails', {
+    url: '/johndoe',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/patientDetails.html',
+        controller: 'patientDetailsCtrl'
+      }
+    }
+  })
+
+  .state('menu.patientDetails2', {
+    url: '/janedoe',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/patientDetails2.html',
+        controller: 'patientDetails2Ctrl'
+      }
+    }
+  })
+
+  .state('loggedOut', {
+    url: '/logout',
+    templateUrl: 'templates/loggedOut.html',
+    controller: 'loggedOutCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
